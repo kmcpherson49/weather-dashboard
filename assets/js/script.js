66cbb147dc60faffb79e47e97d766707267c291a
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-03-13 12:42:34 -0700

    remove preivous styling links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 $("#search-btn").on("click", function (event) {
   event.preventDefault();
   var userInput = $("#search-city").val();
-  console.log(userInput);
+  
+  localStorage.setItem(userInput);
   currentWeather(userInput);
+  loadCity(userInput);
+
 });
+
+var loadCity = function (userInput) {
+    var storedCity = localStorage.getItem(userInput)
+    $("#search-history").val(storedCity);
+}
 //WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
 //var apiUrl = "https://api.github.com/users/" + user + "/repos";
 function currentWeather(userInput) {
@@ -148,7 +156,10 @@ function forecast(userInput) {
       fifthForecastCard.append(fifthForecastCardBody);
       $("#5-day-forecast").append(fifthForecastCard);
     });
+
 }
+
+loadCity();
 
 //WHEN I view current weather conditions for that city THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
